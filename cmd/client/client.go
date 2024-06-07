12d129d3651a0d40828080a6b27dc1687bb3af79
Weslley Vieira <w3slley@pm.me>
2024-06-07 19:42:34 +0100

    Make client/server connection work and be able to send message

--- cmd/client/client.go
+++ cmd/client/client.go
@@ -2,37 +2,67 @@ package main
 
 import (
 	"bufio"
-	"context"
 	"fmt"
-	"log"
 	"net"
 	"os"
-	"time"
+	"sync"
 )
 
+const (
+	HOST     = "192.168.1.126"
+	PORT     = "8000"
+	PROTOCOL = "tcp"
+	CONN     = HOST + ":" + PORT
+)
+
+var wg sync.WaitGroup
+
 func main() {
-	var d net.Dialer
-	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
-	defer cancel()
-	conn, err := d.DialContext(ctx, "tcp", "localhost:8000")
+	wg.Add(1)
+
+	conn, err := net.Dial(PROTOCOL, CONN)
 	if err != nil {
-		log.Fatalf("Failed to dial: %v", err)
+		fmt.Println(err)
 	}
-	defer conn.Close()
-	Write(conn)
+
+	go Read(conn)
+	go Write(conn)
+
+	wg.Wait()
+}
+
+func Read(conn net.Conn) {
+	reader := bufio.NewReader(conn)
+	for {
+		message, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println("Disconnected")
+			return
+		}
+		fmt.Print(message)
+	}
 }
 
 func Write(conn net.Conn) {
 	reader := bufio.NewReader(os.Stdin)
+	writer := bufio.NewWriter(conn)
+
 	for {
-		line, err := reader.ReadString('\n')
+		message, err := reader.ReadString('\n')
 		if err != nil {
-			fmt.Fprintln(os.Stderr, "error: ", err)
+			fmt.Println(err)
 			os.Exit(1)
 		}
-		if _, err := conn.Write([]byte(line)); err != nil {
-			log.Fatal(err)
+
+		_, err = writer.WriteString(message)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		if err = writer.Flush(); err != nil {
+			fmt.Println(err)
+			os.Exit(1)
 		}
 	}
-
 }
--- cmd/server/server.go
+++ cmd/server/server.go
@@ -1,36 +1,54 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"log"
 	"net"
-	"os"
 )
 
 const (
 	TRANSPORT_PROTOCOL = "tcp"
-	HOST               = "localhost"
+	HOST               = "192.168.1.126"
 	PORT               = "8000"
+	CONN               = HOST + ":" + PORT
 )
 
 func main() {
-	listener, err := net.Listen(TRANSPORT_PROTOCOL, HOST+":"+PORT)
+	connections := make([]net.Conn, 2)
+
+	listener, err := net.Listen(TRANSPORT_PROTOCOL, CONN)
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	defer listener.Close()
+	fmt.Println("Listenning on " + CONN)
+
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
-			log.Fatal(err)
+			log.Println("Error: ", err)
+			continue
 		}
-		fmt.Println("Client connected")
-		go handleConnection(conn)
+		fmt.Println("Client connected!")
+
+		go Read(conn)
+
+		connections = append(connections, conn)
+		//fmt.Println(connections)
 	}
 }
 
-func handleConnection(c net.Conn) {
-	buf := make([]byte, 1024)
-	c.Read(buf)
-	os.Stdout.Write(buf)
+func Read(conn net.Conn) {
+	reader := bufio.NewReader(conn)
+	for {
+		message, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println("Disconnected", err.Error())
+			return
+		}
+		fmt.Print(message)
+
+	}
 }
